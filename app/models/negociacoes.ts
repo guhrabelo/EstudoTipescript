5e881570918fe9c738ca17261cb67fb73b49155a
guhrabelo <gustavo.rabelo.teles01@gmail.com>
2021-09-13 14:23:49 -0300

    Reduçao de codigo e programaçao defensiva

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,13 +1,13 @@
 import { Negociacao } from "./negociacao.js";
 
 export class Negociacoes {
-    private negociacoes: Array<Negociacao> = [];
+    private negociacoes: Negociacao[] = [];
 
     adiciona(negociacoes: Negociacao){
         this.negociacoes.push(negociacoes);
     }
 
-    lista(): ReadonlyArray<Negociacao>{ //Readonly: evita que modificações fora da classe sejam permitidas
+    lista(): readonly Negociacao[]{ //Readonly: evita que modificações fora da classe sejam permitidas
         return this.negociacoes;
     }
 }
